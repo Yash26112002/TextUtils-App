0b5004b6af83bc69f24229b44151f4833a829236
Yash <sangwanyash317@gmail.com>
2022-07-02 10:23:34 +0530

    Updates

---
+++ precache-manifest.0271f93a35656195954816912f852d05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6390a2b51eaefac054e91fc59124b73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d16dcc6f7837bdeb82c",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "04c6a948135bc128eb78",
+    "url": "/static/js/2.0f8b5419.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/static/js/2.0f8b5419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7afe56a3e2c75e7abae",
+    "url": "/static/js/3.7e5fe99d.chunk.js"
+  },
+  {
+    "revision": "3d16dcc6f7837bdeb82c",
+    "url": "/static/js/main.8805508d.chunk.js"
+  },
+  {
+    "revision": "50a2d72722e0a70cee85",
+    "url": "/static/js/runtime-main.9a4142d9.js"
+  }
+]);
